0e04f5feb64e1c264635fe06125496e062b8ef94
pa.sharaev <pa.sharaev@tinkoff.ru>
2023-11-28 16:58:29 +0300

    + complete TaskManager

--- ITMO_DZ_4/main.swift
+++ ITMO_DZ_4/main.swift
@@ -1,9 +1,27 @@
 import Foundation
 
-let ff = ThreadSafeArray<Int>()
-var dd = [1]
-dd.append(1)
-ff.dropFirst()
+let a = Task(name: "a", priority: 10)
+let b = Task(name: "b", priority: 8)
+let c = Task(name: "c", priority: 11)
+let d = Task(name: "d", priority: 11)
 
-let sd = ff[0]
+a.addDependency(b)
+//b.addDependency(a)
+a.addDependency(c)
+d.addDependency(a)
 
+let myQueue = DispatchQueue(label: "my")
+let disp = TaskManager(queue: myQueue)
+
+disp.add(a)
+disp.add(b)
+disp.add(c)
+disp.add(d)
+
+do {
+    try disp.buildTasksSnapshot().execute()
+} catch {
+    print(error)
+}
+
+sleep(1)
